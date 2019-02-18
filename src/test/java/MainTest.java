d337adb5f10656314b4b45a63b53b61c2ef911a0
Djurica Djuricic <djurobrah@gmail.com>
2019-02-18 15:46:19 +0100

    JSON Schema validation added.
    Added a schema which I use for validating JSON in a Rest Assured test.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,14 +1,11 @@
+import io.restassured.http.ContentType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
-
-import io.restassured.module.jsv.JsonSchemaValidator.*;
-
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.junit.Assert.*;
 
 
@@ -43,7 +40,28 @@ public class MainTest
                     get("http://ergast.com/api/f1/2017/circuits.json").
                 then().
                     assertThat().
-                        statusCode(200);
+                        statusCode(200).
+                        and().
+                        contentType(ContentType.JSON);
+        //@formatter:on
+    }
+
+    @Test
+    public void test_commentUsesSchema()
+    {
+        System.out.println(System.getProperty("java.class.path"));
+
+        //@formatter:off
+        given().
+                when().
+                    get("http://localhost:3000/comments/1").
+                then().
+                    assertThat().
+                        statusCode(200).
+                        and().
+                        contentType(ContentType.JSON).
+                        and().
+                        body(matchesJsonSchemaInClasspath("comment-schema.json"));
         //@formatter:on
     }
 }
