13d613c8858716711b36b7c2055c3efc85fe84e0
Djurica Djuricic <djurobrah@gmail.com>
2019-02-19 13:46:17 +0100

    Object mapping & POST request.
    Object converted to JSON and then set as body in a POST request. Works.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,6 +3,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
@@ -28,40 +29,53 @@ public class MainTest
         assertEquals(1, 1);
     }
 
-    //@formatter:off
-    //@formatter:on
-
     @Test
     public void test_StatusCodeShouldBe200()
     {
         //@formatter:off
         given().
-                when().
-                    get("http://ergast.com/api/f1/2017/circuits.json").
-                then().
-                    assertThat().
-                        statusCode(200).
-                        and().
-                        contentType(ContentType.JSON);
+        when().
+            get("http://ergast.com/api/f1/2017/circuits.json").
+        then().
+            assertThat().
+            statusCode(200).
+            and().
+            contentType(ContentType.JSON);
         //@formatter:on
     }
 
     @Test
     public void test_commentUsesSchema()
     {
-        System.out.println(System.getProperty("java.class.path"));
-
         //@formatter:off
         given().
-                when().
-                    get("http://localhost:3000/comments/1").
-                then().
-                    assertThat().
-                        statusCode(200).
-                        and().
-                        contentType(ContentType.JSON).
-                        and().
-                        body(matchesJsonSchemaInClasspath("comment-schema.json"));
+        when().
+                get("http://localhost:3000/comments/1").
+        then().
+                assertThat().
+                    statusCode(200).
+                    and().
+                    contentType(ContentType.JSON).
+                    and().
+                    body(matchesJsonSchemaInClasspath("comment-schema.json"));
+        //@formatter:on
+    }
+
+    @Test
+    public void test_postComment()
+    {
+        Comment comment = new Comment("Telo komentara!", 5);
+        //@formatter:off
+        given().
+                contentType("application/json; charset=utf-8").
+                body(comment).
+        when().
+                post("http://localhost:3000/comments").
+        then().
+                assertThat().
+                    statusCode(201).
+                    and().
+                    body(matchesJsonSchemaInClasspath("comment-schema.json"));
         //@formatter:on
     }
 }
